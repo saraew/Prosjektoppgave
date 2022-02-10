ad67632d9233c7c66d358be6e64f37a6b4e59eb5
Sara Elise Wøllo <sara_ew@hotmail.com>
2022-02-10 13:25:09 +0100

    add functionality to ckeck which packages on markov are old.

--- Code/Main.R
+++ Code/Main.R
@@ -72,8 +72,8 @@ thin_multiplier <- 1
 
 #Checking if the computer runs the same version of R, INLA and inlabru as the original code was run on. 
 sessionInfo()
-update.packages(ask = F)
-
+#update.packages(ask = F)
+old.packages()
 
 # 
 # 
